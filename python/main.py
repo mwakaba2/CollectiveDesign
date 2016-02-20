abecf3139fe9ef839f6db1c62284b95010dbb30a
Mariko Wakabayashi <marimarimekko@gmail.com>
2016-02-20 14:37:40 -0600

    can add live messages to firebase database

--- python/main.py
+++ python/main.py
@@ -1,22 +1,22 @@
 # Citation
 # Source code: http://www.wituz.com/tutorial-make-your-own-twitch-plays-stream.html
 
-
-#Define the imports
+from firebase import firebase
 import twitch
-t = twitch.Twitch();
+t = twitch.Twitch()
 
 #Enter your twitch username and oauth-key below, and the app connects to twitch with the details.
 #Your oauth-key can be generated at http://twitchapps.com/tmi/
-username = "mgw917";
-key = "oauth:edcnnrpfb9j4xi3l5twrxj26g3qai4";
-channel = "geekygoonsquad";
-t.twitch_connect(username, key, channel);
+username = "mgw917"
+key = "oauth:edcnnrpfb9j4xi3l5twrxj26g3qai4"
+channel = "geekygoonsquad"
+t.twitch_connect(username, key, channel)
+firebase = firebase.FirebaseApplication('https://ldw9cv20726.firebaseio-demo.com/', None)
 
 #The main loop
 while True:
     #Check for new mesasages
-    new_messages = t.twitch_recieve_messages();
+    new_messages = t.twitch_recieve_messages()
 
     if not new_messages:
         #No new messages...
@@ -26,4 +26,11 @@ while True:
             #Wuhu we got a message. Let's extract some details from it
             msg = message['message'].lower()
             username = message['username'].lower()
-            print(username + ": " + msg);
+            
+            result = firebase.post(
+                '/PythonChatDemo/Messages',
+                {   
+                    "name": username,
+                    "message": msg,
+                })
+            print "%s inserted to database" % result
